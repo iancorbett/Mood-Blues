7d090af1d09bac33e642c3112ffc205f623d48cb
iancorbett <iancorbett1324@gmail.com>
2024-08-07 18:03:47 -0600

    Update index.js submit form function and event listener
    created submitForm button, a form object that stores user input, stored the object in local storage using json, linked us to next html page and created event listener for the enter button

---
+++ project1/assets/jsCode/index.js
@@ -0,0 +1,16 @@
+const submitForm = (event) => {
+    event.preventDefault();
+    const form = {
+        Mood: document.getElementById('question1').value,
+        Genre: document.getElementById('question2').value
+    }
+  console.log(form);
+
+    localStorage.setItem('userMood&Genre', JSON.stringify(form));
+
+    window.location.href="../playlist.html";
+}
+
+
+
+document.getElementById('enterButton').addEventListener('click', submitForm);
