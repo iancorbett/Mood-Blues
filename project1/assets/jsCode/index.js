354b95cb01585a27032f25ebf134009a445dccd5
zzbrooks <zabirsarwar@yahoo.com>
2024-08-14 21:44:36 -0700

    fixed css render

--- project1/assets/jsCode/index.js
+++ project1/assets/jsCode/index.js
@@ -196,24 +196,24 @@ function displayPlaylist() {
     window.location.replace(playlistUrl);
 }
 
-// API for getting event detail 
-const url = 'https://ticketmasterstefan-skliarovv1.p.rapidapi.com/getSingleEvent';
-const data = new FormData();
+// // API for getting event detail
+// const url = 'https://ticketmasterstefan-skliarovv1.p.rapidapi.com/getSingleEvent';
+// const data = new FormData();
 
-const options = {
-	method: 'POST',
-	headers: {
-		'x-rapidapi-key': '1467dc237dmshb2a40b1132dcfdep18c98cjsn2f712875652d',
-		'x-rapidapi-host': 'Ticketmasterstefan-skliarovV1.p.rapidapi.com'
-	},
-	body: data
-};
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'x-rapidapi-key': '1467dc237dmshb2a40b1132dcfdep18c98cjsn2f712875652d',
+// 		'x-rapidapi-host': 'Ticketmasterstefan-skliarovV1.p.rapidapi.com'
+// 	},
+// 	body: data
+// };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
 
